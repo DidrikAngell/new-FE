f5ba155711a9eb9cc62ecf6331cdbc08c035d3a6
DidrikAngell <didrik.angell@gmail.com>
2024-01-19 11:35:11 -0800

    afsdfwe

--- src/components/TenantDashboard/TenantInbox.js
+++ src/components/TenantDashboard/TenantInbox.js
@@ -15,7 +15,7 @@ export const TenantInbox = () => {
         <div className="w-full rounded-[8px] bg-white">
           <Inbox />
         </div>
-        <div className="min-w-[330px] rounded-[8px] bg-white p-[16px] space-y-[16px] overflow-auto sidebarheight hiddenscrollbar">
+        <div className="w-[330px] rounded-[8px] bg-white p-[16px] space-y-[16px] overflow-auto sidebarheight hiddenscrollbar">
           {currentNFT ? (
             <>
               <div className="text-[20px]">Rental Details</div>
